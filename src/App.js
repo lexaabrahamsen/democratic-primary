7f0f032014f8ce35bd7235192d9afed63f7b8b31
Lexa <lexaraea@gmail.com>
2020-01-07 15:56:35 -0500

    Add vote count

--- src/App.js
+++ src/App.js
@@ -2,55 +2,72 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const candidates = [
+const list = [
   {
     id: 1,
-    name: "Elizabeth Warren"
+    name: "Elizabeth Warren",
+    votes: 0
   },
   {
     id: 2,
-    name: "Bernie Sanders"
+    name: "Bernie Sanders",
+    votes: 0
   },
   {
     id: 3,
-    name: "Pete Buttigieg"
+    name: "Pete Buttigieg",
+    votes: 0
   },
   {
     id: 4,
-    name: "Tom Seyer"
+    name: "Tom Seyer",
+    votes: 0
   },
   {
     id: 5,
-    name: "Joe Biden"
+    name: "Joe Biden",
+    votes: 0
   },
   {
     id: 6,
-    name: "Cory Booker"
+    name: "Cory Booker",
+    votes: 0
   },
   {
     id: 7,
-    name: "Amy Klobuchar"
+    name: "Amy Klobuchar",
+    votes: 0
   },
   {
     id: 8,
-    name: "Andrew Yang"
+    name: "Andrew Yang",
+    votes: 0
   }
 ];
 
 class App extends Component {
+  handleEvent = e => console.log("button clicked for " + e);
+
   render() {
-    return candidates.map(person => (
-      <Candidate key={person.id} name={person.name} />
+    return list.map(person => (
+      <Candidate
+        key={person.id}
+        name={person.name}
+        votes={person.votes}
+        onVote={this.handleEvent}
+      />
     ));
   }
 }
 
 class Candidate extends Component {
+  handleClick = () => this.props.onVote(this.props.name);
+
   render() {
     return (
       <div className="App">
-        {this.props.name}{" "}
-        <button onClick={console.log("button clicked")}>+</button>
+        {this.props.name}
+        <button onClick={this.handleClick}>VOTE</button>
       </div>
     );
   }
