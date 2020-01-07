bd05c18d61b068192ea336e55a84c25bc276cba8
Lexa <lexaraea@gmail.com>
2020-01-07 12:09:48 -0500

    Add vote button

--- src/App.js
+++ src/App.js
@@ -2,15 +2,57 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+const candidates = [
+  {
+    id: 1,
+    name: "Elizabeth Warren"
+  },
+  {
+    id: 2,
+    name: "Bernie Sanders"
+  },
+  {
+    id: 3,
+    name: "Pete Buttigieg"
+  },
+  {
+    id: 4,
+    name: "Tom Seyer"
+  },
+  {
+    id: 5,
+    name: "Joe Biden"
+  },
+  {
+    id: 6,
+    name: "Cory Booker"
+  },
+  {
+    id: 7,
+    name: "Amy Klobuchar"
+  },
+  {
+    id: 8,
+    name: "Andrew Yang"
+  }
+];
+
 class App extends Component {
   render() {
-    return <Candidate name="John Lennon" />;
+    return candidates.map(person => (
+      <Candidate key={person.id} name={person.name} />
+    ));
   }
 }
 
 class Candidate extends Component {
   render() {
-    return <div className="App">{this.props.name}</div>;
+    return (
+      <div className="App">
+        {this.props.name}{" "}
+        <button onClick={console.log("button clicked")}>+</button>
+      </div>
+    );
   }
 }
 
